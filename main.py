c8b780fb477121cb7821ab1ae5a9395517cc8bd8
sxuzmeee <144207608+sxuzmeee@users.noreply.github.com>
2023-11-08 15:47:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#1 (Календарь)
+
+# import calendar
+#
+# print(calendar.isleap(2027))
+#
+# a = print(calendar.weekday(1995, 6, 25))
+#
+# print(calendar.calendar(2023))
+
+
+#2 Репозиторий PyPI. Пакет FuzzyWuzzy.
+
+# from fuzzywuzzy import fuzz
+#
+# r1 = fuzz.ratio('Плохой код на самом деле не плохой.', 'Его просто не так поняли.')
+# r2 = fuzz.ratio('Работает? Не трогай.', 'Работает? Не трогай.')
+# r3 = fuzz.ratio('Работает? Не трогай.', 'Работает? Не трогай!')
+# print(r1, r2, r3)
+#
